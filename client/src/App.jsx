9f8d32b1b136c5ff7c8b6d17977c2c8cc3d2adaa
David0Perez <147769786+David0Perez@users.noreply.github.com>
2024-04-06 14:51:22 -0400

    ./ apollo server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
-import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from '@apollo/client';
-import { Outlet } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from './@apollo/client';
 import { setContext} from '@apollo/client/link/context';
+import { Outlet } from 'react-router-dom';
 
 
 import Navbar from './components/Navbar';
